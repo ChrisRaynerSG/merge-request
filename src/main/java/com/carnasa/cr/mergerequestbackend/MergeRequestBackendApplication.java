5c508fefbca45556c5ca9c5408bfdadedb0fd2f6
Chris <carnasagames@gmail.com>
2025-02-05 13:54:03 +0000

    initial spring boot project commit

---
+++ src/main/java/com/carnasa/cr/mergerequestbackend/MergeRequestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.carnasa.cr.mergerequestbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MergeRequestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MergeRequestBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carnasa/cr/mergerequestbackend/MergeRequestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carnasa.cr.mergerequestbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MergeRequestBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
